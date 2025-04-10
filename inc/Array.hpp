57745d56d07363146594df2e547fad8c48193056
Banana <fotyek.robert63@gmail.com>
2025-04-10 16:50:59 +0200

    skibidi

--- inc/Array.hpp
+++ inc/Array.hpp
@@ -2,7 +2,7 @@
 
 
 template<typename T>
-class Array {
+    class Array {
 protected:
     T* data;
     size_t size;
@@ -14,7 +14,10 @@ public:
     Array& operator=(const Array&);
     T& operator[](size_t idx);
     const T& operator[](size_t idx) const;
-
+    
+    void insert(T* elem, size_t idx);
+    void remove(size_t idx);
+    virtual ~Array();
 };
 template<typename T>
 std::ostream& operator<<(std::ostream&, Array<T>);
