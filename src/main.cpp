25272e117d85363fbc98f291afdedad04f88aa41
banana <fotyek.robert63@gmail.com>
2025-05-26 14:10:58 +0200

    second wind

--- src/main.cpp
+++ src/main.cpp
@@ -18,107 +18,120 @@ int main(int argc, const char** argv)
         
         int select = 0;
         std::cin >> select;
+
+        // validation
+        if (std::cin.fail()) 
+        {
+            std::cerr << "selection format invalid" << std::endl;
+            return 1;
+        }
+
+        // flush stdin
+        char flush[100];
+        std::cin.getline(flush, 100);
+
+        // bounds check
         if (select < 1 || select > 6) 
         {
             std::cout << "invalid selection!" << std::endl;
-            std::cin.ignore(256);
             continue;
         }
+
+
         switch (select)
         {
-        case 1 :
-            sim.listParticles(std::cout);
-            break;
-        
-        case 2:
-            try 
-            {
-                std::cout << "pos (xyz):" << std::endl;
-                Vector pos;
-                std::cin >> pos.x >> pos.y >> pos.z;
+            case 1 :
+                sim.listParticles(std::cout);
+                break;
+            
+            case 2:
+                try 
+                {
+                    std::cout << "pos (xyz):" << std::endl;
+                    Vector pos;
+                    std::cin >> pos.x >> pos.y >> pos.z;
 
-                std::cout << "vel (xyz):" << std::endl;
-                Vector vel;
-                std::cin >> vel.x >> vel.y >> vel.z;
+                    std::cout << "vel (xyz):" << std::endl;
+                    Vector vel;
+                    std::cin >> vel.x >> vel.y >> vel.z;
 
-                std::cout << "mass:" << std::endl;
-                double mass;
-                std::cin >> mass;
+                    std::cout << "mass:" << std::endl;
+                    double mass;
+                    std::cin >> mass;
 
-                std::cout << "charge:" << std::endl;
-                double charge;
-                std::cin >> charge;
+                    std::cout << "charge:" << std::endl;
+                    double charge;
+                    std::cin >> charge;
 
-                std::cout << "gravity (bool) :" << std::endl;
-                bool grav;
-                std::cin >> grav;
+                    std::cout << "gravity (bool) :" << std::endl;
+                    bool grav;
+                    std::cin >> grav;
 
-                sim.addParticle(new Particle(pos, vel, mass, charge, grav));
-            } 
-            catch (...) 
-            {
-                 std ::cout << "bad input!" << std::endl;
-                 continue;
-            }
+                    sim.addParticle(new Particle(pos, vel, mass, charge, grav));
+                } 
+                catch (...) 
+                {
+                    std ::cout << "bad input!" << std::endl;
+                    continue;
+                }
 
-            break;
+                break;
 
-        case 3:
-            std::cout << "which particle? (negative for exit)" << std::endl;
-            int select;
-            std::cin >> select;
-            if (select < 0) break;
+            case 3:
+                std::cout << "which particle? (negative for exit)" << std::endl;
+                int select;
+                std::cin >> select;
+                if (select < 0) break;
 
-            try {sim.removeParticle(select);}
-            catch (std::out_of_range& e)
-            {
-                std::cout << e.what() << std::endl;
-                continue;
-            }
-            break;       
+                try {sim.removeParticle(select);}
+                catch (std::out_of_range& e)
+                {
+                    std::cout << e.what() << std::endl;
+                    continue;
+                }
+                break;       
 
-        case 4:
-            {
-            std::cout << "length, timestep? (l ts)" << std::endl;
-            double length, step;
-            std::cin >> length >> step;
+            case 4:
+                {
+                std::cout << "length, timestep? (l ts)" << std::endl;
+                double length, step;
+                std::cin >> length >> step;
 
-            std::ofstream os("sim.dat", std::ofstream::out | std::ofstream::binary);
-            sim.write(os);
-            for (double t0 = 0; t0 < length; t0 += step)
-            {
-                sim.step(step);
+                std::ofstream os("sim.dat", std::ofstream::out | std::ofstream::binary);
                 sim.write(os);
-            }
-            os.close();
-            }
-            break;
+                for (double t0 = 0; t0 < length; t0 += step)
+                {
+                    sim.step(step);
+                    sim.write(os);
+                }
+                os.close();
+                }
+                break;
 
-        case 5:
-            {
-            std::fstream is;
-            is.open("sim.dat");
-            std::cout << "asdasdasd";
-            std::cout << is.peek();
+            case 5:
+                {
+                std::ifstream is("sim.dat", std::ifstream::binary | std::ifstream::in);
+                std::cout << "asdasdasd";
+                std::cout << is.peek();
 
-            is.seekg(-1, is.end);
-            while (is.peek() != '{') 
-            {
-                std::cout << int(is.tellg());
-                //std::cout << is.peek();
-                is.seekg(-1, is.cur);
-            }
-            sim.read(is);
-            is.close();
-            }
-            break;
+                is.seekg(-1, is.end);
+                while (is.peek() != '{') 
+                {
+                    std::cout << char(is.peek());
+                    //std::cout << is.peek();
+                    is.seekg(-1, is.cur);
+                }
+                sim.read(is);
+                is.close();
+                }
+                break;
 
-        case 6:
-            return 0;
-            break;
+            case 6:
+                return 0;
+                break;
 
-        default:
-            break;
+            default:
+                break;
         }
     }
     return 0;
